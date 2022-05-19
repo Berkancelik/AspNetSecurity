41f02d21924b8854c1a88e77d004f9faad2e1366
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-19 14:03:48 +0300

    CORS

--- MyWeb.Web/Startup.cs
+++ MyWeb.Web/Startup.cs
@@ -23,6 +23,13 @@ namespace MyWeb.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opts =>
+            {
+                opts.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
             services.AddControllersWithViews();
         }
 
@@ -43,7 +50,7 @@ namespace MyWeb.Web
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseCors();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
